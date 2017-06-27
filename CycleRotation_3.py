c4e51af4d5d710621d7d42f9de22863c3ef8a683
Thomas Tien <ttien@hearst.com>
2017-06-27 17:37:14 -0400

    On branch master
    	new file:   CycleRotation_3.py
    		For Cycle Rotation challenge
    		e.g. given list [1, 2, 3, 4, 5]
    			Solution(list, 3) should return it right-rotated 3 places
    			[3, 4, 5, 1, 2]

---
+++ CycleRotation_3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+
+A = [1, 2, 3, 4, 5]
+A = []
+
+def Solution(A, K):
+    for i in range(K):
+        try:
+            z = A.pop()
+            A.insert(0,z)
+        except: 
+            pass
+    return A
+
+cycled = Solution(A, 3)
+print cycled
